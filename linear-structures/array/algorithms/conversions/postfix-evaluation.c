05f5340e540e0da48ca907891ded8f0e10b8ac35
tab-a7 <stravoone@gmail.com>
2022-10-30 13:41:13 +0530

    new: added postfix evaluation using stack

---
+++ linear-structures/array/algorithms/conversions/postfix-evaluation.c
@@ -0,0 +1,60 @@
+#include "stack.c" // include the stack file
+#define MAX 100
+
+int isDigit(char c) { // to check whether the char is a digit or ot
+  if (c >= '0' && c <= '9')
+    return 1;
+  return 0;
+}
+
+int operate(char sym, int op1, int op2) { // perform the operation
+  switch (sym) {
+  case '+':
+    return (op1 + op2);
+  case '-':
+    return (op1 - op2);
+  case '*':
+    return (op1 * op2);
+  case '/':
+    return (op1 / op2);
+  default:
+    printf("ERROR!\nPlease try again!\n");
+    return -1;
+  }
+}
+
+int eval(char exp[]) {                   // take in the postfix expression
+  for (int i = 0; exp[i] != '\0'; i++) { // traverse the string
+    char c = exp[i];                     // pick thr next character
+
+    if (c == ' ') // ignore spaces
+      continue;
+    if (isDigit(c) == 1) { // if the character is a digit
+      push(c - '0');       // push the numeric value of the digit/character
+    } else {               // if chatacter is an operator
+
+      char operator= c;     // get the operator
+      int operand1 = pop(); // get the 1st operand
+      int operand2 = pop(); // get the 2nd operand
+
+      // perform the operation and get the new value
+      int val = operate(operator, operand1, operand2);
+      push(val); // push the new value to stack
+    }
+  }
+  int result =
+      pop(); // at the end there is just one value in the stack => the result
+  return result;
+}
+
+int main() {
+
+  char exp[MAX]; // make an character array of size MAX
+
+  printf("|| This program supports only single digit values and basic "
+         "operations ||\n");
+  printf("Enter expression\n");
+  scanf("%[^\n]", exp); // get the expression from the user
+
+  printf("%d\n", eval(exp)); // print the result
+}
---
+++ linear-structures/array/algorithms/conversions/stack.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define SIZE 100
+
+int stack[SIZE], top = -1;
+
+void push(int x) {
+  if (top == SIZE - 1) {
+    printf("Stack full!\n");
+    return;
+  }
+  stack[++top] = x;
+}
+
+int pop() {
+  if (top == -1) {
+    printf("Stack empty!\n");
+    return -1;
+  }
+  return stack[top--];
+}
+
+void display() {
+  if (top == -1) {
+    printf("Stack empty!\n");
+    return;
+  }
+  for (int i = 0; i <= top; i++) {
+    printf("%d \t", stack[i]);
+  }
+}
