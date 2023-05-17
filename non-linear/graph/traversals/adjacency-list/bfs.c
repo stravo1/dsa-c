b5a92761c7ce729536c1ac07c869c1abcbac5609
tab-a7 <stravoone@gmail.com>
2023-05-17 19:15:52 +0530

    chore: (maybe) more readable code for adj-list BFS

--- non-linear/graph/traversals/adjacency-list/bfs.c
+++ non-linear/graph/traversals/adjacency-list/bfs.c
@@ -42,22 +42,12 @@ int getIndexOf(char needle, char haystack[], int len) {
   exit(0);
 }
 
-int main() {
-  int n;
-  printf("Enter number of nodes: ");
-  scanf("%d", &n);
+void createAdjecencyList(node *list[], char names[], int len) {
 
-  /* get names for all nodes */
-  char names[n];
-  printf("\n|| NAMES SHOULD BE SINGLE CHARACTERS ||\n");
-  for (int i = 0; i < n; i++) {
-    printf("Enter name for node %d: ", i + 1);
-    scanf(" %c", &names[i]);
-  }
-
-  node *list[n];
-  for (int i = 0; i < n; i++) {
-    list[i] = NULL; // initialization
+  /* iterate through all the nodes */
+  for (int i = 0; i < len; i++) {
+    list[i] = NULL;
+    // initialization of linked list for each node
 
     int nOn; // number-of-neighbours
     printf("\nEnter number of neighbours of %c: ", names[i]);
@@ -66,26 +56,31 @@ int main() {
     for (int j = 0; j < nOn; j++) {
       char c;
       printf("Enter the name of the %d(th) neighbour: ", j + 1);
-      scanf(" %c", &c);
+      scanf(" %c", &c); // take character input
 
-      int k = getIndexOf(c, names, n);
+      int k = getIndexOf(c, names, len); // convert to array index
 
-      /* create new node */
+      /* create new node to be inserted at the end of linked list */
       node *new = (node *)(malloc(sizeof(node)));
       new->key = k;
       new->next = NULL;
 
-      node *temp = list[i];
-      if (!temp) { // create the 1st link
-        list[i] = new;
-      } else { // add to the end of the list
-        while (temp->next) {
+      node *temp = list[i]; // start from the 'head'
+
+      if (!temp) {     // if 'head' is null, i.e., linked list has not been
+                       // initialized yes
+        list[i] = new; // assign 'head'
+      } else {         // if linked list exists
+        while (temp->next) { // go to the end of the list
           temp = temp->next;
         }
-        temp->next = new;
+        temp->next = new; // append the new node at the end
       }
     }
   }
+}
+
+void BFS(node *adjList[], char names[], int n) {
 
   /* keeps track of visited nodes */
   int visited[n];
@@ -105,17 +100,37 @@ int main() {
 
   /* until queue is empty */
   while (rear != -1) {
-    int n = dequeue(); // dequeue
-    node *temp = list[n];
-    while (temp) { // check for neighbours
-      int i = temp->key;
-      if (!visited[i]) {
+    int n = dequeue();       // dequeue
+    node *temp = adjList[n]; // get the 'head' of the linked list containing the
+                             // neighbours of the dequeued node
+    while (temp) {           // traverse the linked list till the end
+      int i = temp->key;     // each node is a neighbour of tge dequeued element
+      if (!visited[i]) {     // if not already visited
         enqueue(i);
         visited[i] =
             1; // if unvisited neighbour found enqueue ut and mark it visited
       }
       temp = temp->next;
     }
-    printf("%c\t", names[n]);
+    printf("%c\t", names[n]); // print the dequeued node
   }
+}
+int main() {
+  int n;
+  printf("Enter number of nodes: ");
+  scanf("%d", &n);
+
+  /* get names for all nodes */
+  char names[n];
+  printf("\n|| NAMES SHOULD BE SINGLE CHARACTERS ||\n");
+  for (int i = 0; i < n; i++) {
+    printf("Enter name for node %d: ", i + 1);
+    scanf(" %c", &names[i]);
+  }
+
+  node *list[n]; // declare adjacency list
+
+  createAdjecencyList(list, names, n);
+
+  BFS(list, names, n); // perform BFS on the given adjacency list
 }
